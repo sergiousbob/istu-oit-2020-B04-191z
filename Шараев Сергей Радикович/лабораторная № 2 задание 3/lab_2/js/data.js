1154a28dea3942de2b56728d3a60463df8e26ba4
sergiousbob <65399585+sergiousbob@users.noreply.github.com>
2020-05-20 16:35:47 +0600

    Лабораторная работа № 2

---
+++ Шараев Сергей Радикович/лабораторная № 2 задание 3/lab_2/js/data.js
@@ -0,0 +1,120 @@
+﻿var products = [{
+    id: "1",
+    text: "ЗИП и комплектующие ",
+    
+    items: [{
+        id: "1_1",
+        text: "Комплектующие для ноутбуков",
+        
+        items: [{
+            id: "1_1_1",
+            text: "матрицы",
+            items: [{
+                    id: "1_1_1_1",
+                    text: "N156B6-L0B матрица для ноутбука 15.6, 1366x768 WXGA HD, cветодиодная (LED)",
+                    price: 39,
+                    image: "image/N156B6-L0B матрица для ноутбука 15.6, 1366x768 WXGA HD, cветодиодная (LED).jpg"
+            }, {
+                    id: "1_1_1_2",
+                    text: " MacBook Pro 13 Retina Touch Bar A1989 Mid 2018 Early 2019 Space Gray, для Mid 2018 Early 2019, поставка Apple AASP 661-10037 ",
+                    image: "image/MacBook Pro 13 Retina Touch Bar A1989 Mid 2018 Early 2019.jpg",
+                    price: 485,
+                }]
+            
+        }, {
+            id: "1_1_2",
+            text: "Клавиатуры",
+            
+            items: [{
+                id: "1_1_2_1",
+                text: "PK1314K2A05 клавиатура для ноутбука Lenovo IdeaPad G50-30, G50-45, G50-70, G50-80",
+                image: "image/PK1314K2A05 клавиатура для ноутбука Lenovo IdeaPad G50-30, G50-45, G50-70, G50-80.jpg",
+                price: 6
+            },
+            {
+                id: "1_1_2_2",
+                text: "Hama Covo черный, белый USB slim для ноутбука R1173000",
+                image: "image/Hama Covo черный, белый USB slim для ноутбука R1173000.jpg",
+                price: 6
+            }]
+        }, {
+            id: "1_1_3",
+            text: "твердотельные накопители SSD",
+            
+            items: [{
+                id: "1_1_3_1",
+                text: "Silicon Power S60 240GB (SP240GBSS3S60S25)",
+                
+                image: "image/Silicon Power S60 240GB (SP240GBSS3S60S25).jpg",
+                    price: 100
+            }, {
+                id: "1_1_3_2",
+                text: "Seagate 1TB Expansion (STJD1000400)",          
+                image: "image/Seagate 1TB Expansion (STJD1000400).jpg",
+                    price: 170              
+                }]
+                    
+            }]
+        
+                  
+
+    }, {
+        id: "1_2",
+        text: "Комплектующие для персональных компьютеров",
+        items: [{
+            id: "1_2_1",
+            text: "материнские платы",
+            items: [{
+                id: "1_2_1_1",
+                text: "Gigabyte Z390 Aorus Master",
+                image: "image/Gigabyte Z390 Aorus Master.jpg",
+                price: 340
+            },{
+                id: "1_2_1_2",
+                text: "Asus Rog Maximus XI Heror",
+                image: "image/Asus Rog Maximus XI Hero.jpg",
+                price: 330
+            }]
+        }, {
+            id: "1_2_2",
+            text: "Процессоры",
+            items: [{
+                id: "1_2_2_1",
+                text: "Процессор INTEL Core i9 9900K, BOX (без кулера)",
+                image: "image/Core i9 9900K, BOX (без кулера).jpg",
+                price: 418
+                },{
+                id: "1_2_2_2",
+                text: "Процессор Intel Core i5-9600K",
+                image: "image/Процессор Intel Core i5-9600K.jpg",
+                price: 275
+            
+            }]
+         
+            
+         }]
+    }, {
+        id: "1_3",
+        text: "ЗИП для ремонта офисной оргтехники",
+        items: [{
+            id: "1_3_1",
+            text: "Термопленка (Fuser Film) для HP LaserJet 1010, 1020,1022,1015, 1160, 1320, P 2055, 2035, 2050",
+            image: "image/Термопленка (Fuser Film) для HP LaserJet 1010, 1020,1022,1015, 1160, 1320, P 2055, 2035, 2050.jpg",
+                price: 2
+            }, {
+                id: "1_3_2",
+                text: "Шлейф сканера ADF JC39-00358A для SAMSUNG SCX-4200, SCX-4100, SCX-4220, SCX-4300",
+                image: "image/Шлейф сканера ADF JC39-00358A для SAMSUNG SCX-4200, SCX-4100, SCX-4220, SCX-4300.jpg",
+                price: 5
+            
+        }, {
+            id: "1_3_3",
+            text: "Сканирующая линейка совместимая для Samsung M2020/M2070/M2625/M2670/M2825/SCX-3400/SCX-3405, 0609-001409",
+            image: "image/Сканирующая линейка совместимая для Samsung M2020.jpg",
+                    price: 33
+                
+           
+                }]
+            }]
+        }];
+    
