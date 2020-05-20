ec101dd8a0503088a646535127b6294d8be3f7d4
sergiousbob <65399585+sergiousbob@users.noreply.github.com>
2020-05-20 14:41:45 +0600

    исходная версия первой лабораторной работы

---
+++ Шараев Сергей Радикович/лабораторная работа №1 вариант № 19/лабараторная 1/script.js
@@ -0,0 +1,21 @@
+﻿jQuery(document).ready(function($){
+ $('<style>'+
+ '.scrollTop{ display:none; z-index:9999; position:fixed;'+
+ 'bottom:20px; left:5px; width:88px; height:125px;'+
+ 'background:url(img/up.png) 0 0 no-repeat; }' +
+ '</style>').appendTo('body');
+ var
+ speed = 550,
+ $scrollTop = $('<a href="#" class="scrollTop">').appendTo('body'); 
+ $scrollTop.click(function(e){
+ e.preventDefault();
+ $( 'html:not(:animated),body:not(:animated)' ).animate({ scrollTop: 0}, speed );
+ });
+ 
+ //появление
+ function show_scrollTop(){
+ ( $(window).scrollTop() > 330 ) ? $scrollTop.fadeIn(700) : $scrollTop.fadeOut(700);
+ }
+ $(window).scroll( function(){ show_scrollTop(); } );
+ show_scrollTop();
+});
